803e978a2135532503d6504ec7e49306f5831895
DeniDavidov <163063649+DeniDavidov@users.noreply.github.com>
2024-03-23 04:33:40 +0300

    Update Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -1,9 +1,9 @@
 my_list=['яблоко','персик','вишня','банан','кокос','ананас']  # Списки (класс List)
 print(my_list)
 print (my_list[0])
-print (my_list[5])
+print (my_list[-1])
 print(my_list[2:5])
-my_list [5] = 111
+my_list [2] = 111
 print(my_list)
 my_dict = {'apple': 'яблоко', 'int': 'целые', 1: 'еденица', 'my_list': my_list} # Словари (класс Dict)
 print(my_dict)
